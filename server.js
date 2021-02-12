fa22b0103b65a062fe936721add447f26a6be019
thanathip-sukkulcharoen <thanathip.sukkulcharoen@gmail.com>
2021-02-12 12:16:01 +0700

    adding port

--- server.js
+++ server.js
@@ -51,6 +51,6 @@ app.get('/profile/:id',(req,res) => { profile.handleProfileGet(req, res ,db ,bcr
 app.put('/image',(req,res) => {image.handleImage(req,res,db,bcrypt)});
 app.post('/imageurl',(req,res) => {image.handleApiCall(req,res)});
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 });
